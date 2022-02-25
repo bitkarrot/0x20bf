63f149255a3e114c7dc64909054976482e11b1c8
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-02-25 12:49:17 -0500

    remove gogs and seeder

--- seeder/bitcoin.h
+++
@@ -1,8 +0,0 @@
-#ifndef _BITCOIN_H_
-#define _BITCOIN_H_ 1
-
-#include "protocol.h"
-
-bool TestNode(const CService &cip, int &ban, int &client, std::string &clientSV, int &blocks, std::vector<CAddress>* vAddr, uint64_t& services);
-
-#endif
