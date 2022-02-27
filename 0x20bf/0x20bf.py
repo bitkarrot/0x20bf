b4c4c8695a42afa900ddfd1b5538db5d9669332b
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-02-27 12:51:23 -0500

    remove: twitter_api_keys dependancy from 0x20bf.py

--- 0x20bf/0x20bf.py
+++ 0x20bf/0x20bf.py
@@ -1,24 +1,9 @@
 #!/usr/bin/env python3
-from twitter_api_keys import *
 from imports import *
 from configs import *
 # MESSAGE_TREE is used to create a git topic branch
 # associated with a specific SHA256(GPGR+GPGS) message pair
 global MESSAGE_TREE
-
-api  = TwitterAPI(CAK,CASK,AT,ATS)
-if (LOGGER): print(api)
-
-def tweet_block_time():
-    if BTC_TIME() != OBT:
-        request = api.request('statuses/update', {'status': BTC_UNIX_TIME_MILLIS()})
-        if (LOGGER): logger.info(request)
-        if (request.status_code == 200):
-            logger.info('api.request SUCCESS')
-        else:
-            logger.info('api.request FAILURE')
-    else:
-        logger.info('tweetblock_time() FAILURE')
 
 def searchGPGR(GPGR):
     try:
