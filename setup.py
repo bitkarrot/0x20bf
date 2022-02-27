b5a607be6c82efc2d65f675fd5e63122cd876a89
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-02-27 17:12:08 -0500

    setup.py flake8 compliance

--- setup.py
+++ setup.py
@@ -6,18 +6,21 @@ from setuptools import setup
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
 # string in below ...
+
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
 setup(
-    name = "0x20bf",
-    version = "0.0.1",
-    author = "randymcmillan, bitkarrot",
-    author_email = "randy.lee.mcmillan@gmail.com, bitkarrot@bitcoin.org.hk",
-    description = ("0x20bf: This document proposes an Internet standards track protocol for transporting, broadcasting and syndication of messages over common internet communications channels. The distribution of all documents related to this proposal are unlimited and unencumbered by any LICENSE, but some are included anyway."),
-    license = "Apache License 2.0",
-    keywords = "",
-    url = "http://packages.python.org/0x20bf",
+    name="0x20bf",
+    version="0.0.1",
+    author="randymcmillan, bitkarrot",
+    author_email="randy.lee.mcmillan@gmail.com, bitkarrot@bitcoin.org.hk",
+    description=("0x20bf: This document proposes an Internet standards track protocol for transporting, broadcasting and syndication of messages over common internet communications channels. The distribution of all documents related to this proposal are unlimited and unencumbered by any LICENSE, but some are included anyway."),
+    license="Apache License 2.0",
+    keywords="",
+    url="http://packages.python.org/0x20bf",
     packages=['0x20bf', 'tests'],
     long_description=read('README.md'),
     classifiers=[
@@ -26,4 +29,3 @@ setup(
         "License :: OSI Approved :: Apache License 2.0",
     ],
 )
-
