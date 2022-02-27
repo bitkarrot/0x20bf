845c02e251a8943b8753206816e765740e9d5c3c
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-02-27 17:35:34 -0500

    0x20bf/time_functions.py flake8 compliance

--- 0x20bf/time_functions.py
+++ 0x20bf/time_functions.py
@@ -1,39 +1,40 @@
 #!/usr/bin/env python3
-from configs import *
-import aiohttp
 import asyncio
 import os
-import sys
 import time
-import math
 import shutil
 import blockcypher
-from logger import *
+from logger import logger
 
 from mempool_height import mempool_height
 
+
 async def touch_time(time):
-    f = open(os.getcwd()+"/BLOCK_TIME", "w")
+    f = open(os.getcwd() + "/BLOCK_TIME", "w")
     f.write("" + str(time) + "\n")
     f.close()
 
+
 def move_block_time():
     try:
-        shutil.move(os.getcwd()+"/BLOCK_TIME", os.getcwd()+"/OLD_BLOCK_TIME")
-    except:
+        shutil.move(os.getcwd() + "/BLOCK_TIME", os.getcwd() + "/OLD_BLOCK_TIME")
+    except Exception:
         logger.info("moveblock_time() failed!")
         touch_time()
         pass
+
 
 def getMillis():
     global millis
     millis = int(round(time.time() * 1000))
     return millis
 
+
 def getSeconds():
     global seconds
     seconds = int(round(time.time()))
     return seconds
+
 
 def blockcypher_height():
     try:
@@ -44,9 +45,10 @@ def blockcypher_height():
         f.write("" + blockcypher_height + "\n")
         f.close()
         return int(blockcypher_height)
-    except:
+    except Exception:
         return 0
         pass
+
 
 def BTC_TIME():
     mempool_loop = asyncio.new_event_loop()
@@ -54,10 +56,11 @@ def BTC_TIME():
     # assert int(BTC_TIME) >= int(blockcypher_height())
     return int(BTC_TIME)
 
+
 def BTC_UNIX_TIME_MILLIS():
     global btc_unix_time_millis
     global SESSION_ID
-    btc_unix_time_millis = str(BTC_TIME())+":"+str(getMillis())
+    btc_unix_time_millis = str(BTC_TIME()) + ":" + str(getMillis())
     SESSION_ID = btc_unix_time_millis
     f = open("SESSION_ID", "w")
     f.write("" + SESSION_ID + "\n")
@@ -67,33 +70,29 @@ def BTC_UNIX_TIME_MILLIS():
     f.close()
     return btc_unix_time_millis
 
+
 def BTC_UNIX_TIME_SECONDS():
     global btc_unix_time_seconds
-    btc_unix_time_seconds = str(BTC_TIME())+":"+str(getSeconds())
+    btc_unix_time_seconds = str(BTC_TIME()) + ":" + str(getSeconds())
     return btc_unix_time_seconds
+
 
 def UNIX_TIME_MILLIS():
     global unix_time_millis
     unix_time_millis = str(getMillis())
     return unix_time_millis
 
+
 def UNIX_TIME_SECONDS():
     global unix_time_seconds
     unix_time_seconds = str(getSeconds())
     return unix_time_seconds
 
+
 async def fetch(session, url):
     async with session.get(url) as response:
         return await response.text()
 
-async def mempool_height():
-    async with aiohttp.ClientSession() as session:
-        url = "https://mempool.space/api/blocks/tip/height"
-        height = await fetch(session, url)
-        if (MEMPOOL_LOGGER): logger.info(height)
-        return height
-
 loop = asyncio.new_event_loop()
 loop.run_until_complete(touch_time(BTC_TIME()))
 loop.run_until_complete(mempool_height())
-
