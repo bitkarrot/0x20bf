b988dca6db358d4d129f3cb5c254002c571ba4db
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-02-28 12:06:12 -0500

    0x20bf/imports.py flake8 compliance

--- 0x20bf/imports.py
+++ 0x20bf/imports.py
@@ -1,25 +1,11 @@
 #!/usr/bin/env python3
-# pylint: disable=missing-module-docstring
-# pylint: disable=missing-class-docstring
-# pylint: disable=missing-function-docstring
 import os
 import sys
-import time
-import requests
-import logging
-import hashlib
-from socket import *
-import codecs
-import psutil
-import gmpy2
-import logger
-import logging
-import time_functions
 
 sys.path.append('.')
+sys.path.append("/usr/local/lib/python3.10/site-packages")
 sys.path.append("/usr/local/lib/python3.9/site-packages")
-# sys.path.insert(0, cwdget()+"/pycoin/pycoin")
-os.environ["PYTHONIOENCODING"] = "utf-8"
-import blockcypher
-from TwitterAPI import TwitterAPI
-from mempool_height import *
+sys.path.append("/usr/local/lib/python3.8/site-packages")
+sys.path.insert(0, os.cwdget() + "/0x20bf/p2p")
+sys.path.insert(0, os.cwdget() + "/0x20bf/gnupg")
+# os.environ["PYTHONIOENCODING"] = "utf-8"
