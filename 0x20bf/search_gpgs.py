10b32a0a51f7ba56b49492c53c32267c1db60775
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-02-28 13:56:53 -0500

    add 0x20bf/search_gpgs.py

---
+++ 0x20bf/search_gpgs.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# from configs import HEX_LOGGER
+from twitter_api_keys import CAK, CASK, AT, ATS
+from TwitterAPI import TwitterAPI
+from logger import logger
+from time_functions import BTC_UNIX_TIME_MILLIS
+# from hex_message_digest import HEX_MESSAGE_DIGEST
+
+api = TwitterAPI(CAK, CASK, AT, ATS)
+
+
+def search_gpgs(GPGS):
+    # TODO: refactor asyncio
+    try:
+        global r
+        request = api.request('search/tweets', {'q': GPGS})
+        try:
+            with open(GPGS + ":" + BTC_UNIX_TIME_MILLIS(), 'w+') as f:
+                f.write(request.text)
+                f.close
+        except Exception:
+            logger.info("TRY GPGR FAILED!")
+            pass
+    except Exception:
+        logger.info("GPGR SEARCH FAILED!")
+        pass
+
+
+GPGS = 'BB06757B'  # randymcmillan
+search_gpgs(GPGS)
+# logger.info(GPGS)
