3d7c9d1e3f4d37ef4277fafc0ab40c0c634dc8b5
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-02-28 13:35:21 -0500

    0x20bf/imports.py fix/flake8 compliance

--- 0x20bf/imports.py
+++ 0x20bf/imports.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
-import os
 import sys
 
 sys.path.append('.')
 sys.path.append("/usr/local/lib/python3.10/site-packages")
 sys.path.append("/usr/local/lib/python3.9/site-packages")
 sys.path.append("/usr/local/lib/python3.8/site-packages")
-sys.path.insert(0, os.cwdget() + "/0x20bf/p2p")
-sys.path.insert(0, os.cwdget() + "/0x20bf/gnupg")
-# os.environ["PYTHONIOENCODING"] = "utf-8"
